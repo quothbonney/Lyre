1e6ed6d013fe952a89957f83f6836e49513d31cf
quothbonney <jackie.d.carson@gmail.com>
2022-07-18 13:46:01 -0500

    Functional

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import numpy as np
 import wave
 from scipy.io import wavfile
 from scipy.signal import find_peaks
+from math import floor
 
-violin_wav = "samples/violin_c.wav"
+violin_wav = "samples/chords.wav"
 sax_wav = "samples/sax.wav"
 
 
@@ -34,6 +35,9 @@ class Spectrum:
         f_bins = int(len(self.spectrum)*0.1)
         self.peaks, _ = find_peaks(self.spectrum[:f_bins], distance=30, prominence=10, threshold=0.9)
 
+        self.freqs = []
+        self.isolate(self.peaks)
+
     def plot_magnitude_spectrum(self, title, f_ratio=0.1):
         plt.figure(figsize=(18, 5))
 
@@ -48,10 +52,30 @@ class Spectrum:
         plt.title(title)
         plt.show()
 
+    def isolate(self, pks):
+        conf, others = [], []
+
+        for peak in pks:
+            multiple = peak/pks[0]
+            val = abs(multiple - round(multiple))
+            
+            if val < 0.2: conf.append(val)
+            else: others.append(peak)
+        
+        if len(conf) > 3: self.freqs.append(pks[0])
+
+        if len(others) != 0:
+            self.isolate(others)
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    audio = AudioSignal(violin_wav, duration=1.0, offset=float(1), sample_override=None)
+    audio = AudioSignal(violin_wav, duration=1.0, offset=float(4), sample_override=None)
 
     audio.spectrum.plot_magnitude_spectrum(title=f"{violin_wav} at time 0")
-
-    print(audio.spectrum.peaks / audio.spectrum.peaks[0])
+    
+    print(audio.spectrum.freqs)
